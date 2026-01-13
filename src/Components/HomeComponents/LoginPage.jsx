3a9f65c7a66d4c7e5967eb014894e0f847cdad70
Mobasshir <mobasshirhussain2002@gmail.com>
2026-01-13 23:54:00 +0530

    login page work

---
+++ src/Components/HomeComponents/LoginPage.jsx
@@ -0,0 +1,15 @@
+import React from 'react'
+
+const LoginPage = () => {
+  return (
+    <div>
+        <h1>Customer Login</h1>
+        <div>
+            <input placeholder='Username' type="text" name="name" id="" />
+
+        </div>
+    </div>
+  )
+}
+
+export default LoginPage
